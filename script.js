478fe38ba977a7cae6b3e7c3857c95ed7304fbbb
ALEXANDRE MACHADO <85898396+Alexmachado81@users.noreply.github.com>
2021-09-06 13:33:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   var p = document.getElementById("principal").value;
+   var r = document.getElementById("rate").value;
+   var y = document.getElementById("years").value;
+   var y1 = now.getFullYear();
+   var final = calculo(p,r,y,y1);
+   document.getElementById("result").innerHTML = + final;
+}
+
+function calculo(p,r,y,y1)   {
+   var lucro = p*y*r;
+   var atual = y1+y;
+   var resultado = "If you deposit "+ p
+            "at an interest rate of " + y
+            "you will receive an amount of " + lucro
+            "in the year " + atual;
+return resultado
     
 }
         

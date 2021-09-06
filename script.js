32beaa1521f9932ff1f1a436ffacc9ba27f19403
ALEXANDRE MACHADO <85898396+Alexmachado81@users.noreply.github.com>
2021-09-06 17:19:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 function compute()
 {
-   var p = document.getElementById("principal").value;
-   var r = document.getElementById("rate").value;
-   var y = document.getElementById("years").value;
-   var y1 = now.getFullYear();
-   var final = calculo(p,r,y,y1);
-   document.getElementById("result").innerHTML = + final;
+    var p = document.getElementById("principal").value;
+
+    if(p == "" || p <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var y1 = now.getFullYear();
+    var final = calculo(p,r,y,y1);
+    document.getElementById("result").innerHTML = final;
+    }
+
 }
 
 function calculo(p,r,y,y1)   {
-   var lucro = p*y*r;
+   var lucro = p*y*r /100;
    var atual = y1+y;
-   var resultado = "If you deposit "+ p
+   var resultado = "If you deposit "+ p 
             "at an interest rate of " + y
             "you will receive an amount of " + lucro
             "in the year " + atual;
 return resultado
     
 }
+        
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
+  }
         
